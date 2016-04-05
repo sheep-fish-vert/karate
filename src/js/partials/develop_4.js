e9b9d2c34be2bbad076ac97574f511a359868bf6
Radiksman <radion91@gmail.com>
2016-04-05 11:17:44 +0300

    no message

--- src/js/partials/develop_4.js
+++ src/js/partials/develop_4.js
@@ -1,8 +1,11 @@
 
-
+function formStyle(){
+    $('.lend-form .radio-holder input').styler();
+    $('.lend-form select').styler();
+}
 
 $(document).ready(function(){
-
+    formStyle();
 });
 
 $(window).load(function(){
