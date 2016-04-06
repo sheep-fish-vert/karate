b8c2f5152c8ee94973be355a6e2be420b3256956
Radiksman <radion91@gmail.com>
2016-04-06 10:06:58 +0300

    no message

--- src/js/partials/develop_4.js
+++ src/js/partials/develop_4.js
@@ -3,9 +3,20 @@ function formStyle(){
     $('.lend-form .radio-holder input').styler();
     $('.lend-form select').styler();
 }
+function checkRadioButton(){
+    $('.radio-holder input').change(function(event) {
+       if($('.radio-holder input').is(':checked')) {
+        var el = $(this).closest('.radio-holder').index();
+            var parent = $(this).closest('.form-body');
+            parent.find('.selects-wrap>div').removeClass('active');
+            parent.find('.selects-wrap>div').eq(el).addClass('active');
+        }
+    });
+}
 
 $(document).ready(function(){
     formStyle();
+    checkRadioButton();
 });
 
 $(window).load(function(){
