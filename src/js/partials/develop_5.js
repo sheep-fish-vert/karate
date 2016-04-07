e07226b7eb4feb6ef870b92b0530c3019fb71204
Art Garm <artem.garmatyk@gmail.com>
2016-04-07 11:02:25 +0300

    add

--- src/js/partials/develop_5.js
+++ src/js/partials/develop_5.js
@@ -2,9 +2,17 @@ function parallax(){
     var scrolled = $(window).scrollTop() - $('.fifth-wrap').offset().top;
     $('.magic').css('top', -(scrolled*0.2) + 'px');
 }
+
+function parallax2(){
+    var scrolled = $(window).scrollTop() - $('.third-block').offset().top;
+    $('.magik2').css('top', (scrolled*0.3) + 'px');
+}
+
 $(window).scroll(function(){
     parallax();
+    parallax2();
 });
+
 $(document).ready(function(){
 
 });
